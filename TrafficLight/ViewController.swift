914d84ec72825e691f360752bf4a44165cb6e1c3
Bogdan Radchenko <inroph@mail.ru>
2023-01-31 00:48:34 +0300

    adding logic

--- TrafficLight/ViewController.swift
+++ TrafficLight/ViewController.swift
@@ -18,9 +18,31 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         
+        redSignalView.alpha = 0.3
+        yellowSignalView.alpha = 0.3
+        greenSignalView.alpha = 0.3
+        
+        redSignalView.layer.cornerRadius = redSignalView.frame.width / 2
+        yellowSignalView.layer.cornerRadius = yellowSignalView.frame.width / 2
+        greenSignalView.layer.cornerRadius = greenSignalView.frame.width / 2
     }
 
     @IBAction func changeColor() {
+        
+        if redSignalView.alpha == 1.0 {
+            redSignalView.alpha = 0.3
+            yellowSignalView.alpha = 1.0
+        } else if yellowSignalView.alpha == 1.0 {
+            yellowSignalView.alpha = 0.3
+            greenSignalView.alpha = 1.0
+        } else if greenSignalView.alpha == 1.0 {
+            greenSignalView.alpha = 0.3
+            redSignalView.alpha = 1.0
+        } else {
+            trafficLightButtonTapped.setTitle("NEXT", for: .normal)
+            redSignalView.alpha = 1.0
+        }
+        
     }
     
 }
